de60b517627209e1a1fc2065a41c05a9b40dd05c
DarEini1995 <dareini1995@gmail.com>
2021-08-24 18:34:24 +0300

    interface update

---
+++ Ex04.Menus.Interfaces/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ex04.Menus.Interfaces
+{
+    class MainMenu
+    {
+        private Menu m_Menu;
+        
+        public MainMenu(Menu i_Menu)
+        {
+            m_Menu = i_Menu;
+        }
+        public Menu Menu
+        {
+            get
+            {
+                return m_Menu;
+            }
+        }
+
+    }
+}
---
+++ Ex04.Menus.Interfaces/MenuCommand.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ex04.Menus.Interfaces
+{
+    class MenuCommand : MenuItem
+    {
+        private readonly IExecutable r_Command;
+
+        public MenuCommand(string i_ItemName, IExecutable i_Command, Menu i_PreviousMenu)
+            : base(i_ItemName, i_PreviousMenu)
+        {
+            r_Command = i_Command;
+        }
+
+        internal override void DoWhenMenuItemSelected()
+        {
+            r_Command.Execute();
+        }
+
+    }
+}
